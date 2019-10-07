3d51ba2b321bf85656c289397e52f3c167e27f05
samhuynhle <samhuynhle@gmail.com>
2019-10-07 10:08:43 -0700

    completed

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human rob = new Human("Robby");
+            Human bobby = new Human("Bobby");
+            System.Console.WriteLine("Bobby's Health: " + bobby.Health);
+            rob.Attack(bobby);
+            System.Console.WriteLine("Boddy's Health after: " + bobby.Health);
+        }
+    }
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+        }
+        public Human(string NameInput)
+        {
+            Name = NameInput;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+        public Human(string NameInput, int StrengthInput, int IntelligenceInput, int DexterityInput, int healthInput)
+            {
+                Name = NameInput;
+                Strength = StrengthInput;
+
+            }
+        public int Attack(Human target)
+        {
+            target.health = target.health - ( 5 * this.Strength );
+            System.Console.WriteLine(this.Name + " has attacked " + target.Name);
+            return target.Health;
+        }
+    }
+}
